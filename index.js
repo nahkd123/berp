b9c143f70fb7bfc6bc341e048d82f00d1ca9c8cb
nahkd123 <nahkd123@gmail.com>
2020-03-09 11:12:04 +0700

    Solved the 'text overflow' problem
    Problem and Solution:
    * New line chars on stdout and stderr messed up GUI
      Split stdout and stderr into array
    
    Note: (no note)

--- index.js
+++ index.js
@@ -78,8 +78,8 @@ var Con = {
 };
 function runShellTask(shell) {
     childProcess.exec(shell, (error, stdout, stderr) => {
-        if (stderr) Con.display.push("\x1b[91m" + stderr + "\x1b[0m");
-        Con.display.push(stdout + "\x1b[0m");
+        if (stderr) stderr.split("\n").forEach((a) => {Con.display.push("\x1b[91m" + a + "\x1b[0m");});
+        stdout.split("\n").forEach((a) => {Con.display.push(a + "\x1b[0m");});
     }).on("exit", (code, signal) => {
         Con.display.push("\x1b[90mTask executed (" + code + ")\x1b[0m");
         if (Con.pop) {
